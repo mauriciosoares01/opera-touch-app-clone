ad5734dfc6a103012212cfdcf239eaa9b8f16dc8
mauriciosoares01 <mss.soares@hotmail.com>
2019-12-31 18:25:06 -0300

    set icon animation

--- src/Components/MenuButton/index.js
+++ src/Components/MenuButton/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import {Container, MainButton} from './styles';
+import LottieView from 'lottie-react-native';
 
 function MenuButton() {
+  const menuAnim = useRef(null);
+
+  const StartAnim = () => {
+    menuAnim.current.play(85, 120);
+  };
+
+  useEffect(() => {
+    menuAnim.current.play(1, 70);
+  }, []);
+
   return (
     <Container>
-      <MainButton></MainButton>
+      <MainButton onPressIn={StartAnim} activeOpacity={0.9}>
+        <LottieView
+          ref={menuAnim}
+          source={require('../../assets/Animations/menuButton.json')}
+          loop={false}
+          style={{width: 35, height: 35}}
+        />
+      </MainButton>
     </Container>
   );
 }
--- src/Components/MenuButton/styles.js
+++ src/Components/MenuButton/styles.js
@@ -2,7 +2,11 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View``;
 
-export const MainButton = styled.View`
+export const MainButton = styled.TouchableOpacity.attrs({
+  activeOpatcity: 0,
+})`
+  align-items: center;
+  justify-content: center;
   height: 55px;
   width: 55px;
   background-color: #6d2aff;
